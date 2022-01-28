e56aeac64386708981968be27b2272128b45921b
Francisco Rodriguez <fjrodriguez.353@gmail.com>
2022-01-27 21:00:52 -0300

    feat: improve styles structure and global vars

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,59 +1,27 @@
 import { Link } from 'react-router-dom'
-import styles from './Item.module.css'
-import { Card, Space, Button, Typography } from '@douyinfe/semi-ui'
+import { styles } from './Item.styles.js'
+import { Card, Space, Button, Tag } from '@douyinfe/semi-ui'
 
 export const Item = ({ id, title, image, category }) => {
 	const { Meta } = Card
-	const { Text } = Typography
-	// <div className={styles.item}>
-	//   <div className={styles.card}>
-	//     <Link to={`/products/${id}`}>
-	//       <h1 className={styles.title}> {title}</h1>
-	//       <img className={styles.image} src={image} alt={title} />
-	//       <p>ver detalle</p>
-	//     </Link>
-	//     <Link to={`/category/${category}`}>
-	//       <p>{category}</p>
-	//     </Link>
-	//   </div>
-	// </div>
 
 	return (
 		<Link to={`/products/${id}`}>
 			<Card
 				style={{ maxWidth: 360 }}
-				title={
-					<Meta
-						title={title}
-						style={{
-							whiteSpace: 'nowrap',
-						}}
-					/>
-				}
-				// headerExtraContent={<Text link>More</Text>}
-				cover={
-					<img
-						alt='example'
-						src={image}
-						style={{
-							width: '358px',
-							height: '269px',
-							objectFit: 'contain',
-						}}
-					/>
-				}
+				title={<Meta title={title} style={styles.meta} />}
+				cover={<img alt='example' src={image} style={styles.cover} />}
 				footerLine={true}
-				footerStyle={{ display: 'flex', justifyContent: 'flex-end' }}
+				footerStyle={styles.footer}
 				footer={
-					<Space>
+					<Space style={styles.space}>
 						{/* <Button theme='borderless' type='primary'>
 							Featured Case
 						</Button> */}
-						<Button
-							style={{ backgroundColor: '#ff9100' }}
-							theme='solid'
-							type='primary'
-						>
+						<Link to={`/category/${category}`}>
+							<Tag>{category}</Tag>
+						</Link>
+						<Button style={styles.button} theme='solid' type='primary'>
 							Ver mas
 						</Button>
 					</Space>
