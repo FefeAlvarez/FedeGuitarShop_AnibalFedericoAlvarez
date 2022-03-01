18ee723fbe72d09ffba778e719abbdc99a3a50ee
Federico Alvarez <fefe.alvarez@gmail.com>
2022-03-01 12:42:40 -0300

    fixing code

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { styles } from "./pageStyles/home.styles";
-import { ItemListContainer } from "../components/ItemListContainer/ItemListContainer";
-import { Pagination } from "@douyinfe/semi-ui";
 
 export const Home = () => {
   const [buttonChange, setButtonChange] = useState(true);
